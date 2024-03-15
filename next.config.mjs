b6adb49ac35c23d88418784df910d1a6719872d3
Arkadiusz Chwedczuk <chwedczukarek@gmail.com>
2024-03-15 14:16:35 +0100

    build: add basePath to cfg

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withMDX = createMDX({});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/personal-page",
   pageExtensions: ["tsx", "mdx"],
   output: "export",
 };
